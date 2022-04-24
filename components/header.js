cf91148765ff6c46fd69b1081a4e67fc6789d798
serkan <serkan.urganci@endigitals.com>
2022-04-24 19:16:38 +0300

    install svgr package & header general design

--- components/header.js
+++ components/header.js
@@ -1,24 +1,35 @@
 import Link from "next/link";
+import styles from "../assets/styles/Header.module.scss";
+import ThemeDarkIcon from "../assets/images/icon/theme-dark-icon.svg";
+import ThemeLightIcon from "../assets/images/icon/theme-light-icon.svg";
 
 function Header() {
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Giriş</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/photograph">
-          <a>Fotoğraf</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/notes">
-          <a>Notlar</a>
-        </Link>
-      </li>
-    </ul>
+    <header>
+      <nav className={styles.navWrapper}>
+        <ul className={styles.navList}>
+          <li>
+            <Link href="/">
+              <a>Giriş</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/photograph">
+              <a>Fotoğraf</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/notes">
+              <a>Notlar</a>
+            </Link>
+          </li>
+        </ul>
+        <div className={styles.themeIcons}>
+          <ThemeDarkIcon />
+          <ThemeLightIcon />
+        </div>
+      </nav>
+    </header>
   );
 }
 
